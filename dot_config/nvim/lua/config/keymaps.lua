db2db0df7884b9f2e615e1b2002f2f296bc55ef9
Emlowz <124457306+emlowz@users.noreply.github.com>
2026-04-02 09:52:29 +0100

    Initial import

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,23 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set:
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Keep the selection active while shifting indentation in visual mode.
+map("x", "<", "<gv")
+map("x", ">", ">gv")
+
+-- Clear search highlights with Enter in normal mode.
+map("n", "<CR>", function()
+  vim.cmd.nohlsearch()
+end, { silent = true, desc = "Clear search highlighting" })
+
+-- Move through display lines when text wraps on screen.
+map("n", "j", "gj", { desc = "Down by display line" })
+map("n", "k", "gk", { desc = "Up by display line" })
+
+-- Keep the cursor centered while navigating search results.
+map("n", "n", "nzzzv", { desc = "Next search result" })
+map("n", "N", "Nzzzv", { desc = "Previous search result" })
